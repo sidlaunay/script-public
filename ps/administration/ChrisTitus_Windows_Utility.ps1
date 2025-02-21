298c944da5a9aa3f2963f90df1fcf248e6e2eba7
Siddhartha LAUNAY <129612275+sidlaunay@users.noreply.github.com>
2025-02-21 08:53:20 +0100

    Update ChrisTitus_Windows_Utility.ps1

--- ps/administration/ChrisTitus_Windows_Utility.ps1
+++ ps/administration/ChrisTitus_Windows_Utility.ps1
@@ -6,13 +6,13 @@ $identity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
 $principal = New-Object System.Security.Principal.WindowsPrincipal($identity)
 $isAdmin = $principal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
 
-# Si le script n'est pas exécuté en tant qu'administrateur, il relance PowerShell en mode admin
+# Si ce n'est pas le cas, relance PowerShell avec élévation des privilèges
 if (-Not $isAdmin) {
     Write-Host "Demande d'élévation des privilèges..." -ForegroundColor Yellow
-    Start-Process PowerShell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+    Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
     Exit
 }
 
-# Télécharge et exécute le script de Chris Titus
-Write-Host "Exécution du script The Ultimate Windows Utility..." -ForegroundColor Cyan
-iwr -useb https://christitus.com/win | iex
+# Exécution du script Chris Titus en mode administrateur
+Write-Host "Téléchargement et exécution du script The Ultimate Windows Utility..." -ForegroundColor Cyan
+Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command `"iwr -useb https://christitus.com/win | iex`"" -Verb RunAs
